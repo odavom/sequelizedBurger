7728f4c0d18700a671a134f9a93a0b80aa06feb5
Anthony Mastanduno <anthonymastanduno@anthonys-mbp.lan>
2019-04-20 17:58:49 -0400

    created lockfile as package-lock.json

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', false);
